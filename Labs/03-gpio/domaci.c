531f66400d5c8929836806bf211897251a28ed1f
unknown <vitous5@gmail.com>
2020-10-09 11:35:38 +0200

    'Adding 03

---
+++ Labs/03-gpio/domaci.c
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+
+// Function declaration (prototype)
+uint16_t calculate(uint8_t, uint8_t);
+
+int main(void)
+{
+	uint8_t a = 156;
+	uint8_t b = 14;
+	uint16_t c;
+
+	// Function call
+	c = calculate(a, b);
+
+	while (1)
+	{
+	}
+	return 0;
+}
+
+// Function definition (body)
+int calculate(uint8_t x, uint8_t y)
+{
+	uint16_t result;    // result = x^2 + 2xy + y^2
+
+	result = x*x;
+	result +=2*x*2*y;
+	result+=y*y;
+	return result;
+}
+
+
